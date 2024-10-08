d3bb421cdeb24dad7950e59a1bf44fec93db1be6
s30035 <s30035@pjwstk.edu.pl>
2024-10-08 16:49:53 +0200

    Changed the persons name who'll add the subtractor

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 // TODO: we need to add the missing classes!
 
-// OK, I will add `Adder` and s30035 will add `Subtractor`.
+// OK, I will add `Adder` and doasu will add `Subtractor`.
 
 public class Main {
  public static void main(String[] args) {
