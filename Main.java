5992c1703bc57346e7cfa74130f795a52d616e5e
s30035 <s30035@pjwstk.edu.pl>
2024-10-08 16:25:29 +0200

    Initial project setup

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
 // TODO: we need to add the missing classes!
 
+// OK, I will add `Adder` and s30035 will add `Subtractor`.
+
 public class Main {
  public static void main(String[] args) {
- Adder adder = new Adder();
- System.out.println(adder.add(1, 2));
- Subtractor subtractor = new Subtractor();
- System.out.println(subtractor.subtract(6, 3));
+  Adder adder = new Adder();
+  System.out.println(adder.add(1, 2));
+  Subtractor subtractor = new Subtractor();
+  System.out.println(subtractor.subtract(6, 3));
  }
 }
